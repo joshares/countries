5e8d7183060d1647204a3f25aa8c036a23bda633
Joshua Aregbesola <joshares9991@gmail.com>
2023-03-31 17:40:22 +0100

    gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <Navbar/>
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='home' element={<Home/>}/>
         <Route path='country/:id' element={<SinglePage/>}/>
       </Routes>
     </Router>
