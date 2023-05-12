1dfc08d25a05adb8fded9bc16c8aa8ca1ecc9b29
Joshua Aregbesola <106611280+joshares@users.noreply.github.com>
2023-05-12 09:48:40 +0100

    Update Country.tsx

--- src/components/Country.tsx
+++ src/components/Country.tsx
@@ -19,7 +19,7 @@ export const Country = ({name,population,capital,region,flags}:CountrypropsType)
 
   
   return (
-    <Link to={`/country/${name.common}`} className='my-6 dark:bg-darkColor' >
+    <Link to={`/${name.common}`} className='my-6 dark:bg-darkColor' >
      <div className='bg-white shadow-md w-full dark:bg-darkColor dark:text-darkTextColor '>
       <img src={flags.png} alt='img' className='shadow-lg w-full md:h-44 '/>
       <div className='p-3 flex flex-col gap-2'>
